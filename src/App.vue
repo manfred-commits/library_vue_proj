46b27b098b174c638b7387fc3480bbcb534e1a0e
manfred-commits <manfredipiraino@gmail.com>
2022-05-18 07:52:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <MainHeader/>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader
+  }
+}
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300;400;500;600;700&display=swap');
+
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  font-family: 'Roboto Mono', monospace;
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav id="primary_header_navigation">
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Books</a>
+      </li>
+      <li>
+        <a href="#">Contacts</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+#primary_header_navigation{
+  width:100%;
+  position:sticky;
+  top:0;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+#primary_header nav{
+  
+}
+#primary_header_navigation ul{
+  display: flex ;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: nowrap;
+  align-items: center;
+  list-style: none;
+}
+#primary_header_navigation ul li{
+  padding:1rem;
+  
+}
+#primary_header_navigation ul li a{
+  color: black;
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+</style>
